a58be038abab7be460d2ee4452af281bdd151287
Saurabh Agarwal <ska.agrawal5@gmail.com>
2020-08-11 03:04:48 +0530

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module SmartHome {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires jPDFViewerFX;
+    requires javafx.media;
+    requires java.sql;
+    requires java.datatransfer;
+
+    opens sample;
+}
